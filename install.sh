7a60da6616471dc75d029358246f6a6cafbcc2b6
Jake Boeckerman <jakebman@gmail.com>
2022-08-04 23:40:47 -0600

    shuffle more install/setup apart; better theme

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ apk add --no-cache \
   dumb-init \
   git \
   sudo \
+  vim \
   ncurses \
   tzdata
 cp /usr/share/zoneinfo/${SYSTEM_TZ} /etc/localtime
@@ -20,3 +21,10 @@ cp -R /tmp/bash_it /root/.bash_it
 cp -R /tmp/bash_it ${SERVICE_HOME}/.bash_it
 /root/.bash_it/install.sh --silent
 echo -e "\n# Load bash-completion\n[ -f /usr/share/bash-completion/bash_completion  ] && source /usr/share/bash-completion/bash_completion" >> /root/.bashrc
+
+git clone --depth 1 https://github.com/sstephenson/bats.git /tmp/bats
+  /tmp/bats/install.sh /usr/local
+
+sed -i -e "s/bin\/ash/bin\/bash/" /etc/passwd
+apk del tzdata
+rm -rf /tmp/{.}* /tmp/*
--- setup.sh
+++ setup.sh
@@ -5,11 +5,8 @@
 # fail on first error (set -e)
 set -o errexit
 
-sudo --user ${SERVICE_USER} ${SERVICE_HOME}/.bash_it/install.sh --silent
-echo -e "\n# Load bash-completion\n[ -f /usr/share/bash-completion/bash_completion  ] && source /usr/share/bash-completion/bash_completion" >> /root/.bashrc
-git clone --depth 1 https://github.com/sstephenson/bats.git /tmp/bats
-  /tmp/bats/install.sh /usr/local
 chown -R ${SERVICE_USER}:${SERVICE_USER} ${SERVICE_HOME}
-sed -i -e "s/bin\/ash/bin\/bash/" /etc/passwd
-apk del git tzdata
-rm -rf /tmp/{.}* /tmp/*
+sudo --user ${SERVICE_USER} ${SERVICE_HOME}/.bash_it/install.sh --silent
+echo -e "\n# Load bash-completion\n[ -f /usr/share/bash-completion/bash_completion  ] && source /usr/share/bash-completion/bash_completion" >> ${SERVICE_HOME}/.bashrc
+sudo --user ${SERVICE_USER} bash -i -c bash-it profile load jake-home
+sudo --user ${SERVICE_USER} sed -i -e 's/.*BASH_IT_THEME.*/export BASH_IT_THEME=nwinkler/' ${SERVICE_HOME}/.bashrc
