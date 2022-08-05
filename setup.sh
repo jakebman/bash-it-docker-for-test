fbe4833dcddf0541eec2f444130c71f3cd6d0fe4
Jake Boeckerman <jakebman@gmail.com>
2022-08-05 00:28:48 -0600

    Throwing things at the wall to get bash-it to enable a profile programattically

--- setup.sh
+++ setup.sh
@@ -5,8 +5,16 @@
 # fail on first error (set -e)
 set -o errexit
 
+set -x
+
+
+apk add screen
 chown -R ${SERVICE_USER}:${SERVICE_USER} ${SERVICE_HOME}
 sudo --user ${SERVICE_USER} ${SERVICE_HOME}/.bash_it/install.sh --silent
 echo -e "\n# Load bash-completion\n[ -f /usr/share/bash-completion/bash_completion  ] && source /usr/share/bash-completion/bash_completion" >> ${SERVICE_HOME}/.bashrc
-sudo --user ${SERVICE_USER} bash -i -c bash-it profile load jake-home
+# profile load uses tput and needs a terminal, which breaks if you run:
+# vim setup.sh ; docker build -t foo --progress plain .; docker run --rm --env BASH_IT_THEME=nwinkler -it foo
+# but not if you run
+# vim setup.sh ; docker build -t foo --progress tty .; docker run --rm --env BASH_IT_THEME=nwinkler -it foo
+TERM=xterm-256color sudo --user ${SERVICE_USER} bash -x -i -c bash-it profile load jake-home 2>&1| tee /tmp/foo
 sudo --user ${SERVICE_USER} sed -i -e 's/.*BASH_IT_THEME.*/export BASH_IT_THEME=nwinkler/' ${SERVICE_HOME}/.bashrc
