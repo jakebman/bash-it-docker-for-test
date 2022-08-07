67c9397ab3519564dd68265274517cd7411d1a49
Jake Boeckerman <jakebman@gmail.com>
2022-08-06 21:05:18 -0600

    Fix logging
    I like the plain output. The `set -x` output was too much

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 build: .built
 .built: Dockerfile setup.sh install.sh Makefile
-	docker build -t foo --progress tty .
+	docker build -t foo --progress plain .
 	touch .built
 run: build
 	docker run --rm -it foo
--- setup.sh
+++ setup.sh
@@ -13,5 +13,5 @@ sudo --user ${SERVICE_USER} ${SERVICE_HOME}/.bash_it/install.sh --silent
 echo -e "\n# Load bash-completion\n[ -f /usr/share/bash-completion/bash_completion  ] && source /usr/share/bash-completion/bash_completion" >> ${SERVICE_HOME}/.bashrc
 
 # run this command in a subprocess as SERVICE_USER
-sudo --user "$SERVICE_USER" bash -x -i -c "bash-it profile load jake-home 2>&1| tee /tmp/foo2"
+sudo --user "$SERVICE_USER" bash -i -c "bash-it profile load jake-home 2>&1| tee /tmp/foo2"
 sed -i -e 's/.*BASH_IT_THEME.*/export BASH_IT_THEME=nwinkler/' ${SERVICE_HOME}/.bashrc
