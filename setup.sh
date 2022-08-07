3ca157ca7add38891d75334fadab56f5be2ce589
Jake Boeckerman <jakebman@gmail.com>
2022-08-06 19:10:59 -0600

    enable the profiles (todo: grep calls errors)

--- setup.sh
+++ setup.sh
@@ -8,13 +8,10 @@ set -o errexit
 set -x
 
 
-apk add screen
 chown -R ${SERVICE_USER}:${SERVICE_USER} ${SERVICE_HOME}
 sudo --user ${SERVICE_USER} ${SERVICE_HOME}/.bash_it/install.sh --silent
 echo -e "\n# Load bash-completion\n[ -f /usr/share/bash-completion/bash_completion  ] && source /usr/share/bash-completion/bash_completion" >> ${SERVICE_HOME}/.bashrc
-# profile load uses tput and needs a terminal, which breaks if you run:
-# vim setup.sh ; docker build -t foo --progress plain .; docker run --rm --env BASH_IT_THEME=nwinkler -it foo
-# but not if you run
-# vim setup.sh ; docker build -t foo --progress tty .; docker run --rm --env BASH_IT_THEME=nwinkler -it foo
-TERM=xterm-256color sudo --user ${SERVICE_USER} bash -x -i -c bash-it profile load jake-home 2>&1| tee /tmp/foo
-sudo --user ${SERVICE_USER} sed -i -e 's/.*BASH_IT_THEME.*/export BASH_IT_THEME=nwinkler/' ${SERVICE_HOME}/.bashrc
+
+# run this command in a subprocess as SERVICE_USER
+sudo --user "$SERVICE_USER" bash -x -i -c "bash-it profile load jake-home 2>&1| tee /tmp/foo2"
+sed -i -e 's/.*BASH_IT_THEME.*/export BASH_IT_THEME=nwinkler/' ${SERVICE_HOME}/.bashrc
